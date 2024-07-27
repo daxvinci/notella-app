d59153c13d363ca28a8577b662e915c991cd0716
da vinci <84037196+daxvinci@users.noreply.github.com>
2024-07-27 13:00:39 +0100

    image fixed

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -85,7 +85,7 @@ const Profile = ({user,setUser,mail,propic,setPropic,uId}) => {
         <div className="ml-[100px] md:ml-[200px] lg:ml-[220px] flex md:justify-center items-center h-screen py-4 md:py-0 md:h-[600px]">
             <div className="contain flex w-full lg:w-[55%] flex-col md:flex-row justify-between items-center h-full md:h-[70%] gap-8 lg:gap-16">
                 <div className="flex flex-col items-center left-pro md:w-[40%]">
-                    <div className="pic rounded-full border relative w-[200px] lg:w-[300px] h-[200px] lg:h-[300px] overflow-hidden "><input onChange={handleChange} className="pic-upload absolute top-0 left-0 h-full w-full opacity-0 cursor-pointer" type="file" name="profile" id="propic" /><img className="w-[300px] h-[300px] cursor-pointer" src={propic} alt="user profile"/></div>
+                    <div className="pic rounded-full border relative w-[200px] lg:w-[300px] h-[200px] lg:h-[300px] overflow-hidden "><input onChange={handleChange} className="pic-upload absolute top-0 left-0 h-full w-full opacity-0 cursor-pointer" type="file" name="profile" id="propic" /><img className="w-[300px] h-[300px] object-cover cursor-pointer" src={propic} alt="user profile"/></div>
                     <div className="edit mt-10 bg-sky-500 hover:bg-sky-600 px-3 py-1 rounded-2xl active:translate-y-2 relative"><input onChange={handleChange} className="pic-upload absolute top-0 left-0 h-full w-full opacity-0" type="file" name="profile" id="propic" /> <div className="edit-txt">Edit profile pic</div></div>
                 </div>
                 <div className="right-pro flex flex-col">
